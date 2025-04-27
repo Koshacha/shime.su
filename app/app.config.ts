fed692a67a1159814f7885dc090b988403658e14
Ilya M. <qbub@proton.me>
2025-04-28 00:43:05 +0700

    5 changes by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,5 +1,16 @@
 export default defineAppConfig({
   portfolio: {
     primary: "mint",
+    ruResumeFile: "/cv-ilya-mazunin-vue-ru.pdf",
+    enResumeFile: "/cv-ilya-mazunin-vue.pdf",
+  },
+
+  app: {
+    updateDate: "28.4.2025",
+  },
+
+  contacts: {
+    elsewhere: {},
+    email: "qbub@proton.me",
   },
 });
