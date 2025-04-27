19ed9c160fed0bee30be66ac8cbb47c727e5c21d
Ilya M. <qbub@proton.me>
2025-04-28 01:16:46 +0700

    chore: 1 change by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -10,7 +10,27 @@ export default defineAppConfig({
   },
 
   contacts: {
-    elsewhere: {},
+    elsewhere: {
+      "0": {
+        name: "Github",
+        value: "https://test.com",
+      },
+    },
+
     email: "qbub@proton.me",
+
+    elsewhereLinks: [{
+      name: "Telegram",
+      value: "https://t.me/Koshacha",
+    }, {
+      name: "GitHub",
+      value: "https://github.com/Koshacha",
+    }, {
+      name: "Upwork",
+      value: "https://www.upwork.com/freelancers/~01355186bd65b2bf29",
+    }, {
+      name: "LinkedIn",
+      value: "https://www.linkedin.com/in/koshacha",
+    }],
   },
 });
