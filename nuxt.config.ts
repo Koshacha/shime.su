fdcc6ac3900ba8121a08fcf8a044cc364628e2ce
Ilya M. <qbub@proton.me>
2025-04-27 19:12:01 +0700

    chore: install modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/content',
+    "@nuxt/content",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "motion-v/nuxt",
   ],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
-  compatibilityDate: '2024-04-03',
-})
+  compatibilityDate: "2024-04-03",
+});
