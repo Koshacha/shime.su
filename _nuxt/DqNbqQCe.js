78836197eebe46cf4478f895ce9bfcf4fde83a1a
koshacha <qbub@proton.me>
2025-05-19 18:07:59 +0000

    Deploying to gh-pages from @ koshacha/shime.su@d73f82f2a2e05110b83ce4c244bc58dcfa44ee2c 🚀

---
+++ _nuxt/DqNbqQCe.js
@@ -0,0 +1,1 @@
+import{A as s,d as o,e as a,h as t,t as n,Q as r}from"./CujC71zc.js";const c={class:""},i={class:"text-3xl font-semibold mb-12 text-text-primary"},l={class:"space-y-16"},_=s({__name:"WorkPage",setup(d){return(e,m)=>(a(),o("div",c,[t("h1",i,n(e.$t("work.title")),1),t("div",l,[r(e.$slots,"default")])]))}}),f=Object.assign(_,{__name:"WorkPage"});export{f as default};
