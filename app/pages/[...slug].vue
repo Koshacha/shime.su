965e4cd32db1bdf8e3f754142ab027701882d11a
Ilya M. <qbub@proton.me>
2025-05-23 17:17:47 +0700

    chore: test

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
 const route = useRoute();
 
-const { data: page } = await useAsyncData(route.path, () => {
+const collection = await useAsyncData(route.path, () => {
   return queryCollection("content").path(route.path).first();
 });
 
-watch(page, (newPage) => {
-  console.log("Page not found", route.path);
-  console.log(newPage);
-});
-
-if (!page.value) {
-  // throw createError({
-  //   statusCode: 404,
-  //   statusMessage: "Page not found",
-  //   fatal: true,
-  // });
-} else {
-  useSeoMeta({
-    title: page.value.seo.title ?? page.value.title,
-    description: page.value.seo.description ?? page.value.description,
+if (!collection.data.value) {
+  throw createError({
+    message: "Page not found",
+    statusCode: 404,
   });
 }
+
+const data = toRef(() => collection.data.value!);
+useSeoMeta({ ...data.value.seo });
 </script>
 
 <template>
-  <ContentRenderer v-if="page" :value="page" />
-  <p v-else>not found</p>
+  <ContentRenderer v-if="data" :value="data" />
 </template>
