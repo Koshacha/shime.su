0452f2333f08af3f5567e8f9209ff3581d6f8c4f
koshacha <qbub@proton.me>
2025-05-30 16:17:50 +0000

    Deploying to gh-pages from @ koshacha/shime.su@24fc0b3aa9027ad6f6cc52525e19c945212068ea 🚀

---
+++ _nuxt/7R2DiTHP.js
@@ -0,0 +1,1 @@
+import{_ as s,d as r,i as n,Q as o}from"./By5LxkQQ.js";const t={};function a(e,c){return n(),r("li",null,[o(e.$slots,"default")])}const _=Object.assign(s(t,[["render",a]]),{__name:"ProseLi"});export{_ as default};
