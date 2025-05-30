9a697a52e7465b4b27cf00268f1ccfa7d740472a
Ilya M. <qbub@proton.me>
2025-05-31 00:09:35 +0700

    feat: add nuxt.image comp to gallery

--- app/components/content/Gallery.vue
+++ app/components/content/Gallery.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import { type HTMLAttributes } from "vue";
 
 const props = defineProps<{
   images: string[];
   alts: string[];
+  class?: HTMLAttributes["class"];
 }>();
 
 const container = useTemplateRef("container");
@@ -20,7 +22,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="gap-4 my-8" ref="container">
+  <div
+    class="gap-4 my-8"
+    :class="cn(DEFAULT_CLASS, props.class)"
+    ref="container"
+  >
     <div v-for="(src, i) in images" class="column relative mb-4" key="src">
       <a
         data-fancybox="gallery"
@@ -28,7 +34,12 @@ onMounted(() => {
         :title="alts[i]"
         :data-caption="alts[i]"
       >
-        <img :src :alt="alts[i]" />
+        <nuxt-img
+          :src="src"
+          :alt="alts[i]"
+          class="w-full h-full object-cover rounded-md my-0"
+          sizes="sm:100vw md:50vw"
+        />
       </a>
     </div>
   </div>
