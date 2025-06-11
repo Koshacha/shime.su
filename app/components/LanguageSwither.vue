88b022779ff6aa002e293ece3e3394f958b43c48
Ilya M. <qbub@proton.me>
2025-06-11 22:20:53 +0700

    fix: revert

--- app/components/LanguageSwither.vue
+++ app/components/LanguageSwither.vue
@@ -13,9 +13,9 @@
       ref="target"
       class="absolute right-0 mt-2 w-32 bg-secondary rounded-lg shadow-lg py-1 border border-accent/10"
     >
-      <a
+      <switch-locale-path-link
         v-for="lang in locales"
-        :href="switchLocalePath(lang.code)"
+        :locale="lang.code"
         :key="lang.code"
         class="block w-full px-4 py-2 text-left hover:bg-accent/10 transition-colors text-sm"
         :class="
@@ -23,7 +23,7 @@
         "
       >
         {{ lang.name }}
-      </a>
+      </switch-locale-path-link>
     </div>
   </div>
 </template>
@@ -39,7 +39,6 @@ import { useTemplateRef } from "vue";
 type AvailableLocales = "en" | "ru";
 
 const { locale } = useI18n();
-const switchLocalePath = useSwitchLocalePath();
 
 const locales: Array<{
   code: AvailableLocales;
